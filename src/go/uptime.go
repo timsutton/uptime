308a05c1d6064a24a8fe65d31d35a8f84ba9454d
Timothy Sutton <tim@synthist.net>
2024-03-29 06:39:36 -0400

    Bunch of WIP, add other langs

---
+++ src/go/uptime.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"syscall"
+	"time"
+	"unsafe"
+)
+
+func main() {
+	// Define the sysctl mib (Management Information Base)
+	mib := []int32{1, 21} // CTL_KERN, KERN_BOOTTIME
+
+	// Get the size of the returned data
+	size := uintptr(0)
+	_, _, _ = syscall.Syscall6(syscall.SYS___SYSCTL, uintptr(unsafe.Pointer(&mib[0])), 2, 0, uintptr(unsafe.Pointer(&size)), 0, 0)
+
+	// Define a timeval struct to receive the data
+	tv := syscall.Timeval{}
+
+	// Call sysctl again, this time with a buffer
+	_, _, err := syscall.Syscall6(syscall.SYS___SYSCTL, uintptr(unsafe.Pointer(&mib[0])), 2, uintptr(unsafe.Pointer(&tv)), uintptr(unsafe.Pointer(&size)), 0, 0)
+	if err != 0 {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	// Calculate and print the uptime
+	uptime := time.Since(time.Unix(tv.Sec, int64(tv.Usec*1000)))
+	fmt.Printf("%d\n", int64(uptime.Seconds()))
+}
