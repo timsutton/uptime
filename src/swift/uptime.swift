8c5a8bf192d7b66f3953a07b3f7cc79c7a62c107
Timothy Sutton <tim@synthist.net>
2024-04-04 09:56:04 -0400

    swift builds with bazel

--- src/swift/uptime.swift
+++ src/swift/uptime.swift
@@ -1,20 +1,21 @@
 import Foundation
 
-func fetchAndPrintSystemUptimeInSeconds() {
-    var boottime = timeval()
-    var mib: [Int32] = [CTL_KERN, KERN_BOOTTIME] // Management Information Base (MIB)
-    var size = MemoryLayout<timeval>.stride
+@main
+struct Main {
+    static func main() {
+        var boottime = timeval()
+        var mib: [Int32] = [CTL_KERN, KERN_BOOTTIME]
+        var size = MemoryLayout<timeval>.stride
 
-    let result = sysctl(&mib, u_int(mib.count), &boottime, &size, nil, 0)
-    if result != -1 {
-        let bootDate = Date(
-            timeIntervalSince1970: TimeInterval(boottime.tv_sec) + TimeInterval(boottime.tv_usec) / 1_000_000)
-        let uptimeInSeconds = Int(Date().timeIntervalSince(bootDate))
-        print(uptimeInSeconds) // Directly print the uptime in seconds
-    } else {
-        print("Failed to fetch system uptime")
+        let result = sysctl(&mib, u_int(mib.count), &boottime, &size, nil, 0)
+        if result != -1 {
+            let bootDate = Date(
+                timeIntervalSince1970: TimeInterval(boottime.tv_sec) + TimeInterval(boottime.tv_usec) / 1_000_000)
+            let uptimeInSeconds = Int(Date().timeIntervalSince(bootDate))
+            print(uptimeInSeconds) // Directly print the uptime in seconds
+        } else {
+            print("Failed to fetch system uptime")
+            exit(1)
+        }
     }
 }
-
-// Call the function to print the system uptime in seconds
-fetchAndPrintSystemUptimeInSeconds()
