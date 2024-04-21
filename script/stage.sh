fb6f60fce30b9bf495d7188e52c1b6f2ebc8067f
Timothy Sutton <tim@synthist.net>
2024-04-21 16:43:10 -0400

    ensure executability

--- script/stage.sh
+++ script/stage.sh
@@ -12,4 +12,5 @@ for lang in c go; do
     mkdir -p "artifacts/${platform}/${lang}"
     src_path=$(bazel cquery --ui_event_filters=-info --output=starlark --starlark:expr=target.files_to_run.executable.path //src/${lang}/...)
     cp -v "${src_path}" "artifacts/${platform}/${lang}"
+    chmod -R a+x "artifacts/${platform}/${lang}"
 done
