17a450fe860863eb4aff1c487de93beb76d5bd04
Timothy Sutton <tim@synthist.net>
2024-04-21 16:50:20 -0400

    fix permissions setting

--- script/test.sh
+++ script/test.sh
@@ -8,7 +8,9 @@ if [[ "$(uname -s)" == "Linux" ]]; then
   export platform="linux"
 fi
 
-ls -la artifacts/
+# For whatever reason, the archive tar process doesn't preserve executability on the mode
+chmod -R a+x artifacts
+
 "./artifacts/${platform}/c/uptime"
 "./artifacts/${platform}/go/uptime"
 
