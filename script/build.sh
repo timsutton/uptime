2212fc9c28f6a6e4a5eac6ba85e415c94722262e
Timothy Sutton <tim@synthist.net>
2024-04-21 21:12:23 -0400

    forgot to build rs:linux

--- script/build.sh
+++ script/build.sh
@@ -9,10 +9,12 @@ if [[ "${platform}" == "linux" ]]; then
     bazel run --config=quiet //src/c:uptime
     bazel run --config=quiet //src/go:uptime
     bazel run --config=quiet //src/py:uptime
+    bazel run --config=quiet //src/rs:uptime
 
     bazel build --config=quiet --compilation_mode=opt //src/c:uptime
     bazel build --config=quiet --compilation_mode=opt //src/go:uptime
     bazel build --config=quiet --compilation_mode=opt //src/py:uptime
+    bazel build --config=quiet --compilation_mode=opt //src/rs:uptime
 fi
 
 if [[ "${platform}" == "macos" ]]; then
