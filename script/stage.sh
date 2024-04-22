ad238d474a1c0f3081ead4d03b98255afd530905
Timothy Sutton <tim@synthist.net>
2024-04-21 20:25:42 -0400

    stage

--- script/stage.sh
+++ script/stage.sh
@@ -3,13 +3,18 @@
 set -euo pipefail
 
 platform="macos"
-if [[ "$(uname -s)" == "Linux" ]]; then
+if [ "$(uname -s)" = "Linux" ]; then
   export platform="linux"
 fi
 
-# add rs and swift once those are working on Linux
-for lang in c go; do
-    mkdir -p "artifacts/${platform}/${lang}"
-    src_path=$(bazel cquery --ui_event_filters=-info --output=starlark --starlark:expr=target.files_to_run.executable.path //src/${lang}/...)
-    cp -v "${src_path}" "artifacts/${platform}/${lang}"
+for lang in c go rs swift; do
+  if [ "${platform}" = "linux" ]; then
+    # skip rs and swift on linux for now
+    if [ "${lang}" = "rs" ] || [ "${lang}" = "swift" ]; then
+      continue
+    fi
+  fi
+  mkdir -p "artifacts/${platform}/${lang}"
+  src_path=$(bazel cquery --ui_event_filters=-info --output=starlark --starlark:expr=target.files_to_run.executable.path //src/${lang}/...)
+  cp -v "${src_path}" "artifacts/${platform}/${lang}"
 done
