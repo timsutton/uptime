cdb4b75a517e1a149af5da2480ed85517b7a1c02
Timothy Sutton <tim@synthist.net>
2024-04-21 20:36:29 -0400

    test them all

--- script/test.sh
+++ script/test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -euo pipefail
-set -x
 
 platform="macos"
 if [[ "$(uname -s)" == "Linux" ]]; then
@@ -11,10 +10,4 @@ fi
 # For whatever reason, the archive tar process doesn't preserve executability on the mode
 chmod -R a+x artifacts
 
-"./artifacts/${platform}/c/uptime"
-"./artifacts/${platform}/go/uptime"
-
-# if [[ "${platform}" == "macos" ]]; then
-#   "./artifacts/macos/rs/uptime"
-#   "./artifacts/macos/swift/uptime"
-# fi
+find . -type f -name uptime -print -exec {} \;
