871e3d49ba30ebbf2b7c1ebe754e6697fa2c881d
Timothy Sutton <tim@synthist.net>
2024-04-21 21:05:46 -0400

    rs: linux works

--- script/stage.sh
+++ script/stage.sh
@@ -8,12 +8,10 @@ if [ "$(uname -s)" = "Linux" ]; then
 fi
 
 for lang in c go rs swift; do
-  if [ "${platform}" = "linux" ]; then
-    # skip rs and swift on linux for now
-    if [ "${lang}" = "rs" ] || [ "${lang}" = "swift" ]; then
-      continue
-    fi
+  if [ "${platform}" = "linux" ] && [ "${lang}" = "swift" ]; then
+    continue
   fi
+
   mkdir -p "artifacts/${platform}/${lang}"
   src_path=$(bazel cquery --ui_event_filters=-info --output=starlark --starlark:expr=target.files_to_run.executable.path //src/${lang}/...)
   cp -v "${src_path}" "artifacts/${platform}/${lang}"
