d507e0a5c540ba11a989838256f51558673dfa83
Timothy Sutton <tim@synthist.net>
2024-04-28 10:01:16 -0400

    Use sysctlbyname only on darwin

--- src/rb/uptime.rb
+++ src/rb/uptime.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 
-require 'ffi'
-
-module Sysctl
-  extend FFI::Library
-  ffi_lib FFI::Library::LIBC
-  attach_function :sysctlbyname, [:string, :pointer, :pointer, :pointer, :ulong], :int
-
-  class Timeval < FFI::Struct
-    layout :tv_sec, :long,
-           :tv_usec, :int32
-  end
-end
-
 current_os = Gem::Platform.local.os
 
 if current_os == 'darwin'
+  require 'ffi'
+  module Sysctl
+    extend FFI::Library
+    ffi_lib FFI::Library::LIBC
+    attach_function :sysctlbyname, [:string, :pointer, :pointer, :pointer, :ulong], :int
+
+    class Timeval < FFI::Struct
+      layout :tv_sec, :long,
+             :tv_usec, :int32
+    end
+  end
+
   len = FFI::MemoryPointer.new(:uint)
   len.write_uint(Sysctl::Timeval.size)
   boottime = Sysctl::Timeval.new
   Sysctl.sysctlbyname('kern.boottime', boottime, len, nil, 0)
   puts Time.now.to_i - boottime[:tv_sec]
+
 elsif current_os == 'linux'
   puts File.read('/proc/uptime').split.first.to_i
 end
