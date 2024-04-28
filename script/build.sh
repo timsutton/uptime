c3e7283d1abb9a79378c44b9d79bb2f79087288f
Timothy Sutton <tim@synthist.net>
2024-04-28 15:48:24 -0400

    try matrix for build

--- script/build.sh
+++ script/build.sh
@@ -5,22 +5,12 @@ set -euo pipefail
 # shellcheck source=deps.sh
 source ./script/deps.sh
 
-query_linux="kind(.*_binary, //src/... except //src/swift/...)"
-query_macos="kind(.*_binary, //src/...)"
-
+target_lang="${1}"
 if [[ "${platform}" == "linux" ]]; then
     # Note, on Linux we may need to set a couple extra things here to support ruby-build:
     export JAVA_HOME="$(dirname $(dirname $(realpath $(which javac))))"
     # export LANG="en_US.UTF-8"
-    query="${query_linux}"
 fi
 
-if [[ "${platform}" == "macos" ]]; then
-    query="${query_macos}"
-fi
-
-bazel query "${query}" | xargs bazel build --compilation_mode=opt
-
-for tgt in $(bazel query "${query}"); do
-    bazel run --config=quiet "${tgt}"
-done
+bazel build --compilation_mode=opt "//src/${target_lang}:uptime"
+bazel run --config=quiet "//src/${target_lang}:uptime"
