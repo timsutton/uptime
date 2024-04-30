d2df5307727cabfc05c5ee9709499475b3cca638
Timothy Sutton <tim@synthist.net>
2024-04-30 06:16:37 -0400

    update deps

--- script/deps.sh
+++ script/deps.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+BAZELISK_VERSION=1.19.0
+SWIFT_VERSION=5.10
+
+function install_bazelisk() {
+  if command -v bazelisk >/dev/null; then
+    return
+  fi
+
+  bazelisk_version="${BAZELISK_VERSION}"
+  arch=amd64
+  if [[ "$(uname -m)" == "aarch64" ]]; then
+    arch=arm64
+  fi
+  bazelisk_path="$(mktemp -d)"
+  curl -sfL -o "${bazelisk_path}/bazelisk" \
+    "https://github.com/bazelbuild/bazelisk/releases/download/v${bazelisk_version}/bazelisk-linux-${arch}"
+  cp "${bazelisk_path}/bazelisk" "${bazelisk_path}/bazel"
+  chmod +x "${bazelisk_path}/*"
+  export PATH="${bazelisk_path}:${PATH}"
+}
+
 # Can potentially optimize this based on what rules_swift's CI does for Linux:
 # https://github.com/bazelbuild/rules_swift/blob/master/.bazelci/presubmit.yml#L13-L19
 function install_swift_for_linux() {
-  swift_version="5.10"
+  swift_version="${SWIFT_VERSION}"
   ubuntu_version=$(awk -F= '/DISTRIB_RELEASE/ {print $2}' /etc/lsb-release)
   # Because Swift Linux releases put a 'aarch64' in the URL for Arm releases, and nothing at all for x86_64
   arch="$(uname -m)"
@@ -16,6 +37,11 @@ function install_swift_for_linux() {
   fi
 
   install_dir="${HOME}/.cache/swift-${swift_version}-${arch}"
+  if [[ -d "${install_dir}" ]] && [[ -x "${install_dir}/usr/bin/swiftc" ]]; then
+    export PATH="${install_dir}/usr/bin:${PATH}"
+    return
+  fi
+
   mkdir -p "${install_dir}"
 
   pushd "${install_dir}" || exit
@@ -27,12 +53,16 @@ function install_swift_for_linux() {
 }
 
 export platform="macos"
+
 if [[ "$(uname -s)" == "Linux" ]]; then
   if [[ "$(awk -F= '/DISTRIB_ID/ {print $2}' /etc/lsb-release)" != "Ubuntu" ]]; then
     echo "On Linux, only Ubuntu platform is supported for building (for now)" >&2
     exit 1
   fi
   export platform="linux"
+
+  # TODO: on vanilla Ubuntu we will also need curl, JDK (like openjdk-17-jdk-headless) and build-essential, etc.
+  install_bazelisk
   install_swift_for_linux
 fi
 
