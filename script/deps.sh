ccd707adbea2fffc59e23f83a18865ceabcfc4b6
Timothy Sutton <tim@synthist.net>
2024-05-01 20:58:24 -0400

    improve deps.sh script for Ubuntu environment

--- script/deps.sh
+++ script/deps.sh
@@ -17,8 +17,29 @@ function install_bazelisk() {
   curl -sfL -o "${bazelisk_path}/bazelisk" \
     "https://github.com/bazelbuild/bazelisk/releases/download/v${bazelisk_version}/bazelisk-linux-${arch}"
   cp "${bazelisk_path}/bazelisk" "${bazelisk_path}/bazel"
-  chmod +x "${bazelisk_path}/*"
+  chmod +x "${bazelisk_path}"/*
   export PATH="${bazelisk_path}:${PATH}"
+}
+
+install_apt_packages() {
+  apt-get update
+  if ! command -v curl >/dev/null; then
+    apt-get install -y curl
+  fi
+
+  if ! command -v javac >/dev/null; then
+    apt-get install -y openjdk-17-jdk-headless
+  fi
+
+  # what's a good way to detect if we need basic things like libc6-dev?
+  # ..and could we get away with installing a lot fewer packages than all of build-essential?
+  # ..zlib1g-dev is needed for GraalVM native-image
+  # ..libffi/libyaml is needed for Ruby build
+  apt-get install -y \
+    build-essential \
+    libffi-dev \
+    libyaml-dev \
+    zlib1g-dev
 }
 
 # Can potentially optimize this based on what rules_swift's CI does for Linux:
@@ -61,7 +82,12 @@ if [[ "$(uname -s)" == "Linux" ]]; then
   fi
   export platform="linux"
 
-  # TODO: on vanilla Ubuntu we will also need curl, JDK (like openjdk-17-jdk-headless) and build-essential, etc.
+  # A Linux CI runner (e.g. GHA) already has other common dev tools installed, but in
+  # case this looks like some other Ubuntu host, then run some additional required apt installs.
+  if [[ -z "${CI:-}" ]]; then
+    install_apt_packages
+  fi
+
   install_bazelisk
   install_swift_for_linux
 fi
