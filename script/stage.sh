defc8a5245ebb66074b8fab25f64a7fc43299118
Timothy Sutton <tim@synthist.net>
2024-05-03 12:33:56 -0400

    Update comment

--- script/stage.sh
+++ script/stage.sh
@@ -17,21 +17,13 @@ for lang in c go kt rs swift zig; do
   mkdir -p "${STAGING_DIR}/${platform}/${lang}"
 
   # Run this query to just get the runnable executable path
-  # we squelch progress output and even errors, because other lib type targets
+  #
+  # We squelch progress output and even errors, because other lib type targets
   # have no executable path which could get returned by `target.files_to_run.executable.path`
   #
-  # We also grep for an output exe that exactly matches 'uptime', because at least the kt
-  # target can have multiple executables.
-
-  # TODO: maybe this whole approach is flawed, and instead of doing //src/${lang} loop, we should
-  #       just take the same target list from our original build query!
-
-  # echo "DEBUG: cquery output"
-  # bazel cquery \
-  #   --noshow_progress \
-  #   --output=starlark \
-  #   --starlark:expr='target.files_to_run.executable.path' \
-  #   //src/${lang}/...
+  # TODO: instead of doing //src/${lang} loop, we should just take the same target list from our original build query in build.sh
+  # also TODO: doing the above could probably allow us to remove the -error/info ui_event_filter because we wouldn't
+  # be including other targets under ...
 
   output_exe_path=$(bazel cquery \
     --noshow_progress \
