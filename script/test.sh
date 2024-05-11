8e6bbaf543cca2bf2bb9dabc472ff86f28a43869
Timothy Sutton <tim@synthist.net>
2024-05-11 15:02:22 -0400

    Add Bazel tests to test.sh

--- script/test.sh
+++ script/test.sh
@@ -5,7 +5,16 @@ set -euo pipefail
 # shellcheck source=common.sh
 source ./script/common.sh
 
-# For whatever reason, the archive tar process doesn't preserve executability on the mode
-chmod -R a+x "${STAGING_DIR}"
+function execute_built_binaries() {
+    # For whatever reason, the archive tar process doesn't preserve executability on the mode
+    chmod -R a+x "${STAGING_DIR}"
+    find "${STAGING_DIR}" -type f -name uptime -print -exec {} \;
+}
 
-find "${STAGING_DIR}" -type f -name uptime -print -exec {} \;
+# At this point these are only lint-type checks
+function run_bazel_tests() {
+    bazel test --test_output=errors //...
+}
+
+execute_built_binaries
+run_bazel_tests
