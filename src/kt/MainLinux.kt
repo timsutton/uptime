25541fea34aeb5a7a2bce39649e7829678fe5d61
Timothy Sutton <tim@synthist.net>
2025-01-02 00:46:25 -0500

    ktfmt (#140)

--- src/kt/MainLinux.kt
+++ src/kt/MainLinux.kt
@@ -1,21 +1,16 @@
-import java.io.BufferedReader
 import java.io.File
-import java.io.InputStreamReader
-import java.time.Instant
-import java.util.concurrent.TimeUnit
 
 fun main() {
-    try {
-        val lines = File("/proc/stat").readLines()
-        val btimeLine = lines.first { it.startsWith("btime") }
-        val bootTimeSeconds = btimeLine.split(" ")[1].toLong()
+  try {
+    val lines = File("/proc/stat").readLines()
+    val btimeLine = lines.first { it.startsWith("btime") }
+    val bootTimeSeconds = btimeLine.split(" ")[1].toLong()
 
-        val currentTimeSeconds = System.currentTimeMillis() / 1000
-        val uptimeSeconds = currentTimeSeconds - bootTimeSeconds
+    val currentTimeSeconds = System.currentTimeMillis() / 1000
+    val uptimeSeconds = currentTimeSeconds - bootTimeSeconds
 
-        println("$uptimeSeconds")
-
-    } catch (e: Exception) {
-        e.printStackTrace()
-    }
+    println("$uptimeSeconds")
+  } catch (e: Exception) {
+    e.printStackTrace()
+  }
 }
