c6e5aa53498cd8380b74dc4d289344d4a4b26cc4
Timothy Sutton <tim@synthist.net>
2025-01-19 18:36:48 -0500

    update note re: chmod (#148)

--- script/test.sh
+++ script/test.sh
@@ -5,11 +5,12 @@ set -euo pipefail
 # shellcheck source=common.sh
 source ./script/common.sh
 
-# For whatever reason, the archive tar process doesn't preserve executability on the mode
-# TODO: re-check this assumption, this seems like would be very wrong
+# For whatever reason, the archive tar process doesn't preserve executability on the mode,
+# so we must mark things as executable.
 chmod -R a+x "${STAGING_DIR}"
 
 # We don't use `find -exec` here because we want the script to error immediately if any of them fail
+# shellcheck disable=SC2044
 for bin in $(find "${STAGING_DIR}" -type f -name uptime); do
 	echo "Executing ${bin}"
 	$bin
