4e1bb2838c40a8ffbe419b0eaa1cdf236351c4fb
Timothy Sutton <tim@synthist.net>
2025-03-22 11:51:57 -0400

    WIP

--- src/rs/uptime.rs
+++ src/rs/uptime.rs
@@ -1,37 +1,23 @@
 extern crate libc;
-
-use std::mem::zeroed;
+extern crate sysctl;
 
 #[cfg(target_os = "macos")]
 fn uptime() {
-    use libc::{sysctlbyname, timeval};
-    use std::ptr;
+    use sysctl::Sysctl;
+
     use std::time::{SystemTime, UNIX_EPOCH};
 
-    unsafe {
-        let mut boottime: timeval = zeroed();
-        let mut len: libc::size_t = std::mem::size_of::<timeval>() as libc::size_t;
-        let name = std::ffi::CString::new("kern.boottime").unwrap();
+    let boottime = sysctl::Ctl::new("kern.boottime").unwrap();
 
-        if sysctlbyname(
-            name.as_ptr(),
-            &mut boottime as *mut _ as *mut _,
-            &mut len,
-            ptr::null_mut(),
-            0,
-        ) == 0
-        {
-            let now = SystemTime::now()
-                .duration_since(UNIX_EPOCH)
-                .expect("Time went backwards")
-                .as_secs() as i64;
+    let bt_val = boottime.value();
 
-            let uptime_seconds = now - boottime.tv_sec;
-            println!("{}", uptime_seconds);
-        } else {
-            eprintln!("Failed to get system uptime");
-        }
-    }
+    let now = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards")
+        .as_secs() as i64;
+
+    let uptime_seconds = now - bt_val;
+    println!("{}", uptime_seconds);
 }
 
 #[cfg(target_os = "linux")]
