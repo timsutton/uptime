a37b1a958dd9227f1fd81c519c9196cd8c469d66
Timothy Sutton <tim@synthist.net>
2026-01-04 11:43:51 -0500

    DRY build script using enable_platform_specific_config

--- script/build.sh
+++ script/build.sh
@@ -19,11 +19,11 @@ fi
 source ./script/deps.sh
 
 bazel info
-bazel build --config="${PLATFORM}" //...
+bazel build //...
 
 # At this point these are only lint-type checks
-bazel test --config="${PLATFORM}" --test_output=errors //...
+bazel test --test_output=errors //...
 
 for tgt in $(bazel query "${BINARIES_QUERY}"); do
-	bazel run --config=quiet --config="${PLATFORM}" "${tgt}"
+	bazel run --config=quiet "${tgt}"
 done
