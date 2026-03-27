ca57fff5e2e81965b079596f43536035f67192a1
Timothy Sutton <tim@synthist.net>
2026-03-27 10:42:25 +0100

    Avoid Darwin ctypes setup on Linux imports

--- src/py/uptime.py
+++ src/py/uptime.py
@@ -14,20 +14,21 @@ class timeval(Structure):
     ]
 
 
-libc.sysctlbyname.argtypes = [ctypes.c_char_p, ctypes.c_void_p, ctypes.POINTER(c_size_t), ctypes.c_void_p, c_size_t]
-libc.sysctlbyname.restype = ctypes.c_int
-libc.time.argtypes = [ctypes.c_void_p]
-libc.time.restype = c_long
-
-
 def get_system_uptime_darwin():
+    sysctlbyname = libc.sysctlbyname
+    sysctlbyname.argtypes = [ctypes.c_char_p, ctypes.c_void_p, ctypes.POINTER(c_size_t), ctypes.c_void_p, c_size_t]
+    sysctlbyname.restype = ctypes.c_int
+
+    libc.time.argtypes = [ctypes.c_void_p]
+    libc.time.restype = c_long
+
     # Create an instance of timeval
     tv = timeval()
     # The size of the timeval structure
     size = c_size_t(sizeof(tv))
 
     # Get the system uptime using sysctlbyname
-    result = libc.sysctlbyname(b"kern.boottime", byref(tv), byref(size), None, 0)
+    result = sysctlbyname(b"kern.boottime", byref(tv), byref(size), None, 0)
 
     if result != 0:
         # If there is an error (non-zero result), raise an exception
@@ -41,11 +42,20 @@ def get_system_uptime_darwin():
     return uptime_seconds
 
 
-system = platform.uname().system
-if system == "Linux":
-    with open("/proc/uptime", "r") as f:
-        uptime_seconds = int(float(f.readline().split()[0]))
-        print(uptime_seconds)
+def main():
+    system = platform.uname().system
+    if system == "Linux":
+        with open("/proc/uptime", "r") as f:
+            uptime_seconds = int(float(f.readline().split()[0]))
+            print(uptime_seconds)
+        return
 
-if system == "Darwin":
-    print(get_system_uptime_darwin())
+    if system == "Darwin":
+        print(get_system_uptime_darwin())
+        return
+
+    raise NotImplementedError(f"unsupported platform: {system}")
+
+
+if __name__ == "__main__":
+    main()
